80683cb5584a03aba1a2a21eaa1f5a28bb3ed82f
Rain Weng <tangyuw99@gmail.com>
2025-10-27 06:16:49 +0800

    feat: add MySQL database connection setup

---
+++ internal/infrastructure/mysql/database.go
@@ -0,0 +1,40 @@
+package mysql
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DatabaseConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	DBName   string
+}
+
+func NewDatabase(config DatabaseConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.Username,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	log.Println("Database connected successfully")
+	return db, nil
+}
